ccf679419264ffe34d02b1519bb415c6abd7b32e
yurim3 <w2103@e-mirim.hs.kr>
2023-07-29 10:03:30 +0900

    nav box color

---
+++ src/Navigation.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Navigation = () => {
+  return (
+    <div style={{ backgroundColor: '#598C58', height: '60px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+    </div>
+  );
+};
+
+export default Navigation;
